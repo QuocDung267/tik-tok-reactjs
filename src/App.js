94c063afa39c1263264cafd2d39bde9edc3a2219
QuocDung267 <quocdung2672000@gmail.com>
2024-01-03 22:52:44 +0700

    change scss

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import HomePage from './page/Home';
-import NewsPage from './page/News';
-import ContactPage from './page/Contact';
 
 export default function App() {
     return (
         <div className="App">
-            <nav>
-                <ul>
-                    {/* change */}
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/news">News</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            {/* Routes đặt nơi xảy ra sử thay đổi định tuyến */}
-            <Routes>
-                {/* chưa nhiều route nhỏ để định nghĩa ra component */}
-                {/* path sẽ đối chiếu với trình duyệt để render ra component */}
-                <Route path="/" element={<HomePage />} />~
-                <Route path="/news" element={<NewsPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-            </Routes>
+            <h1>React JS</h1>
         </div>
     );
 }
